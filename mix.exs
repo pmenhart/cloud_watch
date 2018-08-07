353f96b9fc838032d669217d178ebda4f9fe3e8d
Jake Morrison <jake@cogini.com>
2018-08-07 07:38:58 -0500

    Add explicit hackney dep

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule CloudWatch.Mixfile do
      {:aws, "~> 0.5.0", optional: true},
      {:ex_aws, "~> 2.0"},
      {:poison, "~> 3.0"},
+     {:hackney, "~> 1.0"},
      {:credo, "~> 0.4.13", only: :dev},
      {:mock, "~> 0.2.0", only: :test},
      {:ex_doc, ">= 0.0.0", only: :dev}]
