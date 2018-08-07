db5218aa344ee748aa15cb373144ca68d2d2ed3f
Jake Morrison <jake@cogini.com>
2018-08-07 22:36:28 +0000

    Add deps directly

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule CloudWatch.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :hackney]]
+    [extra_applications: [:logger]]
   end
 
   # This makes sure your factory and any other modules in test/support are compiled
@@ -37,9 +37,9 @@ defmodule CloudWatch.Mixfile do
   defp deps do
     [
      # {:aws, "~> 0.5.0", optional: true},
-     # {:ex_aws, "~> 2.0"},
-     {:poison, "~> 3.0"},
-     {:hackney, "~> 1.0"},
+     {:httpoison, "~> 0.11"},
+     {:poison, "~> 3.1"},
+     {:hackney, "~> 1.8"},
      {:credo, "~> 0.4.13", only: :dev},
      {:mock, "~> 0.2.0", only: :test},
      {:ex_doc, ">= 0.0.0", only: :dev}]
